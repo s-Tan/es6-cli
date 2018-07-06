8a35e281f4ee7992107215d9bc663646d2ecbb8a
shiyecheng <15190746108@163.com>
2018-07-06 13:55:01 +0800

    add es6 module function

--- app/source/index.js
+++ app/source/index.js
@@ -1,3 +1,8 @@
+import {print, message} from "./tpl";
+
+const int = 1;
 let a = [2, 3];
-[a[0], a[1]] = [a[1], a[0]];
-console.log(a);
+let x = [a[0], a[1]] = [a[1], a[0]];
+print(int);
+print(x);
+print(message);
---
+++ app/source/tpl.js
@@ -0,0 +1,7 @@
+export function print(val) {
+    console.log('this is print func', val)
+};
+export const message = {
+    name: 'haha',
+    age: 20
+};
